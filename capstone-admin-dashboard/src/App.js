1d9017da44f24f41cd104c11588a305a49bd384c
yonatanski <phpcodeyg@gmail.com>
2022-04-26 21:36:11 +0200

    nav bar and side bar

--- capstone-admin-dashboard/src/App.js
+++ capstone-admin-dashboard/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./Components/navbar/NavBar"
+import "./App.css"
+
+import Sidebar from "./Components/sidebar/Sidebar"
+
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <div className="container">
+          <Sidebar />
+        </div>
+      </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
